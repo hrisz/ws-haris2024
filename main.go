da3afa79482116148d5c3bfd71a5ac48f4601d68
hrisz <harissaefuloh@gmail.com>
2024-04-14 21:24:09 +0700

    KOMENTAR

--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"log"
 
-	"iteung/config"
+	"github.com/hrisz/ws-haris2024/config"
 
 	"github.com/aiteung/musik"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 
 
-	"iteung/url"
+	"github.com/hrisz/ws-haris2024/url"
 
 	"github.com/gofiber/fiber/v2"
 )
